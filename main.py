b207c384905bcf74fe5302f5379227c52933787d
JonatanGonzalez0 <3005959480101@ingenieria.usac.edu.gt>
2021-09-14 19:57:48 -0600

    update

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def carga_Menu():
     #botones de las diferentes opciones
     
     filemenu.add_command(label="Cargar Archivo", command=cargarArchivo)
-    filemenu.add_command(label="Analizar", command=extraerInfoTokens)
     filemenu.add_command(label="Reporte Tokens HTML", command=reporteTokensHTML)
     filemenu.add_command(label="Reporte Errores HTML", command=reporteErroresHTML)
     filemenu.add_command(label="Salir", command=salir)
@@ -66,9 +65,9 @@ def cargarArchivo():
 
         entrada = archivo.read()
         entradaPrincipal = entrada
-        analizadorLexico() 
-        messagebox.showinfo("Correcto","Se cargo el archivo lfp correctamente")  
-        
+        analizadorLexico()
+        extraerInfoTokens() 
+
     
         #mainloop()
     except:
@@ -79,10 +78,6 @@ def analizadorLexico():
     global analizador
         
     analizador.Scanner(entradaPrincipal)
-        
-    #analizador.imprimir()
-    print('---------------------------errores ----------------------------------')
-    analizador.imprimirErrores()
         
 def extraerInfoTokens():
     
@@ -222,14 +217,17 @@ def extraerInfoTokens():
             valoresCombo.append(img.titulo)
 
         combo_imagenes["values"]=valoresCombo
+        
+        btn_Original = tk.Button(app,text="Analizar",font=("Courier", 15),command=analizarImagen)
+        btn_Original.place(x=325, y=150)
             
         messagebox.showinfo("Correcto","Analisis completado con exito y sin errores lexicos") 
     else:
-        messagebox.showwarning("Error","Analisis completado con exito se encontraron errores lexicos")              
+        messagebox.showwarning("Error","Analisis completado con éxito y HAY errores lexicos")              
       
     analizador.imprimirErrores() 
-  
-def cargarBotones():
+ 
+def cargarBotones(): 
     btn_Original = tk.Button(app,text="Original",font=("Courier", 15),command=Original)
     btn_Original.place(x=30, y=250)
     
@@ -242,6 +240,116 @@ def cargarBotones():
 
     btn_doubleMirror = tk.Button(app,text="Double Mirror",font=("Courier", 15),command=doubleMirror)
     btn_doubleMirror.place(x=30, y=550)   
+
+def analizarImagen():
+    global combo_imagenes
+    global existeImagen
+    global canvas
+    imagenGrafica = combo_imagenes.get()
+    
+    
+    for imagen in imagenes:
+        if imagen.titulo==imagenGrafica:
+            imagenEncontrada = imagen
+            continue
+    
+    ANCHO = imagenEncontrada.ancho
+    ALTO = imagenEncontrada.alto
+    FILAS = imagenEncontrada.filas
+    COLUMNAS = imagenEncontrada.columnas
+    
+    TamX = int(ANCHO/COLUMNAS)
+
+    TamY = int(ALTO/FILAS)
+    if existeImagen == True:
+        canvas.destroy()
+        
+    canvas = Canvas(app,width=ANCHO,height=ALTO,bg="#ffffff")
+    canvas.pack()
+    img = PhotoImage(width=ANCHO, height=ALTO)
+    canvas.create_image(0,0, image=img, anchor=NW)
+
+    for indice in range(len(imagenEncontrada.celdas)):
+        celTemp = imagenEncontrada.celdas[indice]
+        if celTemp.bool is TRUE:
+
+            inicioX = (celTemp.x)*TamX
+            inicioY = (celTemp.y)*TamY
+            for i in range(inicioX,inicioX+TamX):
+                for j in range(inicioY,inicioY+ TamY):
+                    img.put(celTemp.colorHex,(i,j))
+    existeImagen = True  
+    Carpeta_Raiz = os.path.dirname(os.path.abspath(__file__))
+    img.write( Carpeta_Raiz+'\originales\\'+imagenEncontrada.titulo+'.png', format='png')
+    titulo = imagenEncontrada.titulo.upper()
+    rutaHtml = Carpeta_Raiz + f'\\reporteAnalisis\\analisis{imagenEncontrada.titulo}.html'
+    archivo = open(rutaHtml,"w")
+    archivo.write("<html><head><title> Analisis </title></head>")
+    archivo.write("<body style=\"background-color:  #e5e7e9  ;font-family:courier;font-size:20px;font-style:courier;\" ><br> ")
+    archivo.write(f'<h2 align=\"center\"> Titulo imagen : {titulo} </h2>') 
+    
+    archivo.write("<br><h1 align=\"center\">  Caracteristicas Imagen </h1>") 
+    archivo.write("<table align=\"center\" border=\"1\" WIDTH=\"40%\"><tr><th>Caracteristica </th><th>Valor</th></tr>")
+    
+    archivo.write("<tr><td align=\"center\">ANCHO </td> <td align=\"center\"> "+ str(imagenEncontrada.ancho)+ " </td></tr>")
+    archivo.write("<tr><td align=\"center\">ALTO </td> <td align=\"center\"> "+ str(imagenEncontrada.alto)+ " </td></tr>")
+    archivo.write("<tr><td align=\"center\">FILAS </td> <td align=\"center\"> "+ str(imagenEncontrada.filas)+ " </td></tr>")
+    archivo.write("<tr><td align=\"center\">COLUMNAS </td> <td align=\"center\"> "+ str(imagenEncontrada.columnas)+ " </td></tr>")
+    
+    for filtro in imagenEncontrada.filtros:
+        archivo.write("<tr align=\"center\"><td>FILTRO </td> <td> "+ filtro.upper()+ " </td></tr>")
+        
+    archivo.write("</table>")
+    
+    Carpeta_Raiz = os.path.dirname(os.path.abspath(__file__))
+    
+    rutaOriginal = Carpeta_Raiz+'\originales\\'+imagenEncontrada.titulo+'.png'
+    
+    archivo.write("<br><h2 align=\"center\"> Original </h2>") 
+    archivo.write(f'<div align="center"><img  src=\"{rutaOriginal}\"></div>')
+    
+    
+    if "mirrorx" in imagenEncontrada.filtros:
+        rutaImagen = Carpeta_Raiz+"\originales\\"+imagenEncontrada.titulo+'.png'
+        im = Image.open(rutaImagen)
+        im_mirror = ImageOps.mirror(im)
+        im_mirror.save(Carpeta_Raiz+'\mirrorX\\'+imagenEncontrada.titulo+'.png', quality=95)
+        
+        rutaMirrorX = Carpeta_Raiz+'\mirrorX\\'+imagenEncontrada.titulo+'.png'
+        
+        archivo.write("<br><h2 align=\"center\"> MIRROR X </h2>") 
+        archivo.write(f'<div align="center"><img  src=\"{rutaMirrorX}\"></div>')
+        
+    if "mirrory" in imagenEncontrada.filtros:
+        rutaImagen = Carpeta_Raiz+"\originales\\"+imagenEncontrada.titulo+'.png'
+        im = Image.open(rutaImagen)
+        im_flip = ImageOps.flip(im)
+        im_flip.save(Carpeta_Raiz+'\mirrorY\\'+imagenEncontrada.titulo+'.png', quality=95)
+        
+        rutaMirrorY = Carpeta_Raiz+'\mirrorY\\'+imagenEncontrada.titulo+'.png'
+        
+        archivo.write("<br><h2 align=\"center\"> MIRROR Y </h2>") 
+        archivo.write(f'<div align="center"><img  src=\"{rutaMirrorY}\"></div>')
+        
+    if "doublemirror" in imagenEncontrada.filtros:
+        rutaImagen = Carpeta_Raiz+"\originales\\"+imagenEncontrada.titulo+'.png'
+        
+        im = Image.open(rutaImagen)
+        im_mirror = ImageOps.mirror(im)
+        im_double = ImageOps.flip(im_mirror)
+        im_double.save(Carpeta_Raiz+'\doubleMirror\\'+imagenEncontrada.titulo+'.png', quality=95)
+        
+        rutaMirrorDouble = Carpeta_Raiz+'\doubleMirror\\'+imagenEncontrada.titulo+'.png'
+        
+        archivo.write("<br><h2 align=\"center\"> DOUBLE MIRROR </h2>") 
+        archivo.write(f'<div align="center"><img  src=\"{rutaMirrorDouble}\"></div>')
+    
+    archivo.write("</body></html>")
+    archivo.close()
+    #abrir automaticamente el html en el navegador
+    canvas.destroy()
+    webbrowser.open(rutaHtml,new=2)
+    
 
 def Original():
     global combo_imagenes
@@ -288,7 +396,9 @@ def Original():
     mainloop()
     
 def reporteTokensHTML():
-    archivo = open("REPORTE_TOKENS.html","w")
+    Carpeta_Raiz = os.path.dirname(os.path.abspath(__file__))
+    rutaHtml = Carpeta_Raiz + "\\reporteTokens\\reporte_Tokens.html"
+    archivo = open(rutaHtml,"w")
     archivo.write("<html><head><title> REPORTE DE TOKENS </title></head>")
     archivo.write("<body style=\"background-color:  #e5e7e9  ;font-family:courier;font-size:20px;font-style:courier;\" ><br> ")
     
@@ -304,13 +414,15 @@ def reporteTokensHTML():
     archivo.write("</body></html>")
     archivo.close()
     #abrir automaticamente el html en el navegador
-    webbrowser.open('file://' + os.path.realpath("REPORTE_TOKENS.html"),new=2)
+    webbrowser.open(rutaHtml,new=2)
     
 def reporteErroresHTML():
     if analizador.sePuedeGenerar()==True:
         messagebox.showwarning("ATENCION","No hay errores reportados en la entrada")   
     else:
-        archivo = open("REPORTE_ERRORES.html","w")
+        Carpeta_Raiz = os.path.dirname(os.path.abspath(__file__))
+        rutaHtml = Carpeta_Raiz + "\\reporteTokens\\reporte_Errores.html"
+        archivo = open(rutaHtml,"w")
         archivo.write("<html><head><title> REPORTE DE ERRORES </title></head>")
         archivo.write("<body style=\"background-color:  #e5e7e9  ;font-family:courier;font-size:20px;font-style:courier;\" ><br> ")
         
@@ -326,7 +438,7 @@ def reporteErroresHTML():
         archivo.write("</body></html>")
         archivo.close()
         #abrir automaticamente el html en el navegador
-        webbrowser.open('file://' + os.path.realpath("REPORTE_ERRORES.html"),new=2)
+        webbrowser.open(rutaHtml,new=2)
     
         
 def mirrorX():
